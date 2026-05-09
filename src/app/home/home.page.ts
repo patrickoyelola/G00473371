19f352ac69d4f90c8dca7aacad4ece08bac8e0d1
Patrick Oyelola <1patrickoyelola@gmail.com>
2026-05-09 12:25:26 +0100

    Implemented trending movies on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonImg
+} from '@ionic/angular/standalone';
+
+import { CommonModule } from '@angular/common';
+import { MovieService } from '../services/movie';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonImg
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  movies: any[] = [];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.loadTrendingMovies();
+  }
+
+  loadTrendingMovies() {
+    this.movieService.getTrendingMovies().subscribe((response: any) => {
+      this.movies = response.results;
+      console.log(this.movies);
+    });
+  }
+
+  getImage(path: string) {
+    return this.movieService.getImageUrl(path);
+  }
 }

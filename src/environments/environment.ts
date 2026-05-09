922cc058ca0b84933e4b182110cbc2986a436d56
Patrick Oyelola <1patrickoyelola@gmail.com>
2026-05-09 11:51:00 +0100

    Added TMDB API configuration and movie service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '824ab8731c460f957270603ec17275f3',
+  baseUrl: 'https://api.themoviedb.org/3',
+  imageUrl: 'https://image.tmdb.org/t/p/w500'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
